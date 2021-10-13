4cbee6febde02ff5fce18bb0f1bae21e721bed3c
Michael Paulsen <michael.j.paulsen@gmail.com>
2021-10-13 18:33:24 +0000

    cleared up the whitespce formatted it better.

--- choices.cpp
+++ choices.cpp
@@ -1,14 +1,9 @@
 #include "choices.h"
 
-
-
-
-
 void GoRight(char* action, int& count){
   char** act = &action; 
   count++;
   if(count == 1){
-       
     printf("you choose to go right you see a glimmer of gold.\nYou continue to go down the hall.\nYou reach the end of the hall you see a sea of gold \n you can go through the gold or you can walk around it \n");
     MakeChoice(*act, "Do you walk around the gold (around)or do you go through it (through)", "around", count);
   }
@@ -23,10 +18,10 @@ void GoRight(char* action, int& count){
   }
   printf("%d", count);
 }
+
 void GoLeft(char* action, int& count){ 
   count++;
   if(count == 1){ 
-     
     char** act = &action;
     printf("you go left \n as you continue going down the path that you are going down you start to feel warm \n do you turn back (back) or continue:\n");
     std::cin >> *act;
@@ -36,6 +31,7 @@ void GoLeft(char* action, int& count){
     printf("you choose to go through the gold it starts to rattle.\n you notice that it is starting to get hot in the room, a dragon appears behind you.\n\t YOU DIE,\t the end "); 
   }
 }
+
 void MakeChoice( char* action, const char* prompt, const char* positiveOutCome, int& count ){
   char ** act = &action;
  
